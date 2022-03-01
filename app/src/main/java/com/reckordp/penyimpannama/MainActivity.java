cb6029ad92dc71847a003f29c386f5d95c4d2b04
Reckordp <rmuhammad134@gmail.com>
2022-03-01 15:15:49 +0700

    Menambahkan fitur reset

--- app/src/main/java/com/reckordp/penyimpannama/MainActivity.java
+++ app/src/main/java/com/reckordp/penyimpannama/MainActivity.java
@@ -7,9 +7,11 @@ import android.app.AlertDialog;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -30,46 +32,81 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        byte[] cbuf = new byte[2048];
-        String cbufString = (new String()).concat(TEKS_MENYAPA);
         pegangNama = new File(getFilesDir(), FILE_NAMA);
+        ulangiLayout();
 
         if (pegangNama.exists()) {
-            try {
-                FileInputStream buff = new FileInputStream(pegangNama);
-                if (buff.read(cbuf, 0, (int)FILE_NAMA.length()) > 0) {
-                    cbufString = cbufString.concat(new String(cbuf, StandardCharsets.UTF_8))
-                        .concat(AKHIRAN_SAPA);
-                }
-                namaDitemukan(cbufString);
-                buff.close();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+            tampilkanTeksMenyapa();
         } else {
-            View vw = inflateBadan(R.layout.mengetik);
-            ViewGroup.LayoutParams params = vw.getLayoutParams();
-            params.width = getWindow().getAttributes().width;
-            vw.setLayoutParams(params);
-
-            EditText ketikan = (EditText)findViewById(R.id.ketikan_nama);
-            ketikan.setOnEditorActionListener((v, actionId, event) -> {
-                if ((actionId & EditorInfo.IME_MASK_ACTION) != 0) {
-                    String teksKetikan = v.getText().toString();
-                    try {
-                        FileOutputStream fos = new FileOutputStream(pegangNama);
-                        fos.write(teksKetikan.getBytes(StandardCharsets.UTF_8));
-                        fos.close();
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                    return false;
-                }
-                return false;
-            });
+            tampilkanEditor();
         }
+    }
+
+    private void ulangiLayout() {
+        setContentView(R.layout.activity_main);
+        Button mengulang = findViewById(R.id.mengulang_layout);
+        mengulang.setOnClickListener(v -> {
+            pegangNama.delete();
+            tampilkanEditor();
+            v.setVisibility(View.GONE);
+        });
+    }
+
+    private void kosongkanFrame() {
+        ViewGroup vg = findViewById(R.id.interaksi);
+        vg.removeAllViews();
+
+        runOnUiThread(() -> {
+            vg.invalidate();
+            vg.requestLayout();
+        });
+    }
+
+    private void tampilkanTeksMenyapa() {
+        kosongkanFrame();
+        byte[] cbuf = new byte[2048];
+        String cbufString = (new String()).concat(TEKS_MENYAPA);
+
+        try {
+            FileInputStream buff = new FileInputStream(pegangNama);
+            if (buff.read(cbuf, 0, (int)FILE_NAMA.length()) > 0) {
+                cbufString = cbufString.concat(new String(cbuf, StandardCharsets.UTF_8))
+                        .concat(AKHIRAN_SAPA);
+            }
+            namaDitemukan(cbufString);
+            buff.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        Button mengulang = findViewById(R.id.mengulang_layout);
+        mengulang.setVisibility(View.VISIBLE);
+    }
+
+    private void tampilkanEditor() {
+        kosongkanFrame();
+        View vw = inflateBadan(R.layout.mengetik);
+        ViewGroup.LayoutParams params = vw.getLayoutParams();
+        params.width = getWindow().getAttributes().width;
+        vw.setLayoutParams(params);
+
+        EditText ketikan = (EditText)findViewById(R.id.ketikan_nama);
+        ketikan.setOnEditorActionListener((v, actionId, event) -> {
+            if ((actionId & EditorInfo.IME_MASK_ACTION) != 0) {
+                String teksKetikan = v.getText().toString();
+                try {
+                    FileOutputStream fos = new FileOutputStream(pegangNama);
+                    fos.write(teksKetikan.getBytes(StandardCharsets.UTF_8));
+                    fos.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                ulangiLayout();
+                tampilkanTeksMenyapa();
+                return false;
+            }
+            return false;
+        });
     }
 
     @Override
